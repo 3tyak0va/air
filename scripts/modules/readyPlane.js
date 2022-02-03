b89a66d345de05314ed86822c3e04cd709f6d402
3tyak0va <bloody_lily@ukr.net>
2022-02-03 17:03:50 +0200

    changes in js

--- scripts/modules/readyPlane.js
+++ scripts/modules/readyPlane.js
@@ -1,9 +1,27 @@
-// const readyPlane = (forms, main) => {
-//     forms forEach((form) => {
-//         form.addEventListener('submit', (event) => {
-//             event.preventDefault();
-//         });
-//     })
-// };
+import airplane from './airplane.js';
 
-// export default readyPlane;
+const readyPlane = (forms, main, tourData) => {
+    const data = [];
+
+    forms.forEach((form) => {
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            for (const element of form.elements) {
+                element.disabled = true;
+            }
+
+            data.push({
+                name: form.name.value,
+                ticket: form.ticket.value,
+            });
+
+            if (forms.length === data.length) {
+                forms.forEach(form => form.remove());
+                airplane(main, data, tourData);
+            }
+        });
+    })
+};
+
+export default readyPlane;
